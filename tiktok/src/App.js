2c322a23f690d318b53e59cd1ed8484372a499e5
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-09-18 13:53:25 +0530

    Creating Ticktiok :)

---
+++ tiktok/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Test</h1>
+      <Video/>
+      <Video/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tiktok/src/Video.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Video.css"
+
+function Video() {
+    return (
+        <div className="video">
+            <video src="https://www.youtube.com/embed/ktjafK4SgWM"></video>
+            {/* */}
+        </div>
+    )
+}
+
+export default Video
